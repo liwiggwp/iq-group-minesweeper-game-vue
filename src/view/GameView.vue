6f42d1b1f86d7a0b1a0632b2ce354ea038c72947
Venera Kodo <liwiggwp@gmail.com>
2025-03-25 22:03:00 +0500

    add set flag and open cell

--- src/view/GameView.vue
+++ src/view/GameView.vue
@@ -4,11 +4,25 @@
     <button @click="$router.push('/')">Вернуться к настройкам</button>
     <div class="board">
       <div v-for="(row, rowIndex) in board" :key="rowIndex" class="row">
-        <div v-for="(cell, colIndex) in row" :key="colIndex" class="cell">
+        <div
+          v-for="(cell, colIndex) in row"
+          :key="colIndex"
+          class="cell"
+          @click="openCell(rowIndex, colIndex)"
+          @contextmenu.prevent="flagCell(rowIndex, colIndex)"
+          :style="{
+            backgroundColor: cell.isOpen ? '#444' : '#ccc',
+          }"
+        >
           <img
-            v-if="cell.isMine"
+            v-if="cell.isMine && cell.isOpen"
             src="@/assets/mine.png"
             class="mine-image"
+          />
+          <img
+            v-if="cell.isFlag && !cell.isOpen"
+            src="@/assets/flag.png"
+            class="flag-image"
           />
         </div>
       </div>
@@ -33,7 +47,11 @@ export default {
       const difficulty = this.$route.query.difficulty || "easy";
       const { rows, cols, mines } = this.difficultySettings[difficulty];
       this.board = Array.from({ length: rows }, () =>
-        Array.from({ length: cols }, () => ({ isMine: false }))
+        Array.from({ length: cols }, () => ({
+          isMine: false,
+          isOpen: false,
+          isFlag: false,
+        }))
       );
       this.initMines(rows, cols, mines);
     },
@@ -49,6 +67,16 @@ export default {
         }
       }
     },
+    openCell(row, col) {
+      const cell = this.board[row][col];
+      if (cell.isOpen || cell.isFlag) return;
+      cell.isOpen = true;
+    },
+    flagCell(row, col) {
+      const cell = this.board[row][col];
+      if (cell.isOpen) return;
+      cell.isFlag = !cell.isFlag;
+    },
   },
   created() {
     this.initBoard();
@@ -59,7 +87,7 @@ export default {
 <style>
 .board {
   display: grid;
-  gap: 2px;
+  gap: 1px;
   margin-top: 20px;
   justify-content: center;
 }
@@ -70,12 +98,18 @@ export default {
 .cell {
   width: 30px;
   height: 30px;
-  background-color: #ccc;
   border: 1px solid #999;
 }
 .mine-image {
   width: 100%;
   height: 100%;
   object-fit: cover;
+  pointer-events: none;
+}
+.flag-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  pointer-events: none;
 }
 </style>
