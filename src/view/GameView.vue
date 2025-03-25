566d5ee83b198028fa645db9d94a543450b69fd2
Venera Kodo <liwiggwp@gmail.com>
2025-03-25 21:17:56 +0500

    add generate mines on board

--- src/view/GameView.vue
+++ src/view/GameView.vue
@@ -4,32 +4,50 @@
     <button @click="$router.push('/')">Вернуться к настройкам</button>
     <div class="board">
       <div v-for="(row, rowIndex) in board" :key="rowIndex" class="row">
-        <div v-for="(cell, colIndex) in row" :key="colIndex" class="cell"></div>
+        <div v-for="(cell, colIndex) in row" :key="colIndex" class="cell">
+          <img
+            v-if="cell.isMine"
+            src="@/assets/mine.png"
+            class="mine-image"
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
   data() {
     return {
       board: [],
       difficultySettings: {
-        easy: { rows: 8, cols: 8 },
-        medium: { rows: 16, cols: 16 },
-        hard: { rows: 16, cols: 32 },
+        easy: { rows: 8, cols: 8, mines: 10 },
+        medium: { rows: 16, cols: 16, mines: 40 },
+        hard: { rows: 16, cols: 32, mines: 100 },
       },
     };
   },
   methods: {
     initBoard() {
       const difficulty = this.$route.query.difficulty || "easy";
-      const { rows, cols } = this.difficultySettings[difficulty];
+      const { rows, cols, mines } = this.difficultySettings[difficulty];
       this.board = Array.from({ length: rows }, () =>
-        Array.from({ length: cols }, () => ({}))
+        Array.from({ length: cols }, () => ({ isMine: false }))
       );
+      this.initMines(rows, cols, mines);
+    },
+    initMines(rows, cols, mines) {
+      let minesPlaced = 0;
+
+      while (minesPlaced < mines) {
+        const row = Math.floor(Math.random() * rows);
+        const col = Math.floor(Math.random() * cols);
+        if (!this.board[row][col].isMine) {
+          this.board[row][col].isMine = true;
+          minesPlaced++;
+        }
+      }
     },
   },
   created() {
@@ -54,5 +72,10 @@ export default {
   height: 30px;
   background-color: #ccc;
   border: 1px solid #999;
+}
+.mine-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 </style>
